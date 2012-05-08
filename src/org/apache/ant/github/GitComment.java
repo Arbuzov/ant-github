416ae46812e913bba3434a1d38520ba20053f482
Arbuzov <arbuzov@mcn.ru>
2012-05-08 12:11:32 +0400

    Signed-off-by: Arbuzov <arbuzov@mcn.ru>

---
+++ src/org/apache/ant/github/GitComment.java
@@ -0,0 +1,11 @@
+package org.apache.ant.github;
+
+public class GitComment extends HttpTask {
+
+	@Override
+	protected String getRequestMethod() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/org/apache/ant/github/GitPulls.java
@@ -0,0 +1,11 @@
+package org.apache.ant.github;
+
+public class GitPulls extends HttpTask {
+
+	@Override
+	protected String getRequestMethod() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
