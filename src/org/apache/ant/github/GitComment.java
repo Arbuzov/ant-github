4398e7ecd6e50192c006c3e8a46cd95028116483
Arbuzov <arbuzov@mcn.ru>
2012-05-08 12:25:04 +0400

    Signed-off-by: Arbuzov <arbuzov@mcn.ru>

--- src/org/apache/ant/github/GitComment.java
+++ src/org/apache/ant/github/GitComment.java
@@ -2,10 +2,12 @@ package org.apache.ant.github;
 
 public class GitComment extends HttpTask {
 
+	private String source="https://api.github.com/";
+	
 	@Override
 	protected String getRequestMethod() {
 		// TODO Auto-generated method stub
-		return null;
+		return "GET";
 	}
 
 }
--- src/org/apache/ant/github/GitUser.java
+++ src/org/apache/ant/github/GitUser.java
@@ -1,9 +1,8 @@
 package org.apache.ant.github;
 
-public class GitPulls extends HttpTask {
+public class GitUser extends HttpTask {
 
-	private String source="https://api.github.com/";
-	
+	private String source="https://api.github.com/user";
 	@Override
 	protected String getRequestMethod() {
 		// TODO Auto-generated method stub
